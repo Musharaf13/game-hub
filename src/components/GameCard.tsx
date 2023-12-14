ec2eed07e2f06f2ef5729521a25230f45ec73f82
musharaf13 <muhammadmusharaf35@gmail.com>
2023-12-14 20:09:06 +0500

    Code Duplication reduced by writing GameCardContainer

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconsList from "./PlatformIconsList";
 import ScoreCritic from "./ScoreCritic";
 import getCropedImage from "../utilities";
-
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -20,20 +20,22 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width={'300px'} borderRadius={10} overflow={"hidden"}>
-      <Image src={getCropedImage(game.background_image)}></Image>
-      <CardBody>
-        <Heading fontSize={"xl"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconsList
-            platforms={game.parent_platforms.map(
-              (platform) => platform.platform
-            )}
-          ></PlatformIconsList>
-          <ScoreCritic score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCropedImage(game.background_image)}></Image>
+        <CardBody>
+          <Heading fontSize={"xl"}>{game.name}</Heading>
+          <HStack justifyContent={"space-between"}>
+            <PlatformIconsList
+              platforms={game.parent_platforms.map(
+                (platform) => platform.platform
+              )}
+            ></PlatformIconsList>
+            <ScoreCritic score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,15 +1,18 @@
 import { Card, CardBody } from "@chakra-ui/card";
 import { Skeleton, SkeletonText } from "@chakra-ui/react";
 import React from "react";
+import GameCardContainer from "./GameCardContainer";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={"300px"} borderRadius={10} overflow={"hidden"}>
-      <Skeleton height={"200px"}></Skeleton>
-      <CardBody>
-        <SkeletonText></SkeletonText>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Skeleton height={"200px"}></Skeleton>
+        <CardBody>
+          <SkeletonText></SkeletonText>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
