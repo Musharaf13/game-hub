c4f6c72ef5afa477c4e55b6ae78322f45b110b0e
musharaf13 <muhammadmusharaf35@gmail.com>
2023-12-15 20:21:04 +0500

    Spinner Loading added in Genres List

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -3,41 +3,50 @@ import useGenres from "../hooks/useGenres";
 import {
   Avatar,
   Box,
+  Center,
   HStack,
   Image,
   List,
   ListItem,
+  Spinner,
   Text,
   VStack,
 } from "@chakra-ui/react";
 import getCropedImage from "../utilities";
 
 const GenresList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+
   return (
     <List padding={"20px"}>
-      {data
-        .filter((genre) => !genre.name.includes(" "))
-        .map((genre) => (
-          <ListItem key={genre.id} padding="5px">
-            <HStack>
-              <Image
-                src={getCropedImage(genre.image_background)}
-                borderRadius="5px"
-                boxSize="32px"
-              ></Image>
-              <Text fontSize="lg">{genre.name}</Text>
-            </HStack>
-          </ListItem>
-        ))}
+      {isLoading ? (
+        <Center height={"100vh"}>
+          <Spinner></Spinner>
+        </Center>
+      ) : (
+        data
+          .filter((genre) => !genre.name.includes(" "))
+          .map((genre) => (
+            <ListItem key={genre.id} padding="5px">
+              <HStack>
+                <Image
+                  src={genre.image_background}
+                  borderRadius="5px"
+                  boxSize="32px"
+                ></Image>
+                <Text fontSize="lg">{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          ))
+      )}
     </List>
-    // <Box key={genre.id}>{genre.name}</Box>
-    // <ul>
-    //   {genres.map((genre) => (
-    //     <li key={genre.id}>{genre.name}</li>
-    //   ))}
-    // </ul>
   );
+  // <Box key={genre.id}>{genre.name}</Box>
+  // <ul>
+  //   {genres.map((genre) => (
+  //     <li key={genre.id}>{genre.name}</li>
+  //   ))}
+  // </ul>
 };
 
 export default GenresList;
